45bc247ae71d69bfd62054fba18b4f970f933f9c
Sindre <sinfr91@gmail.com>
2021-09-22 22:35:59 +1200

    added animated knob functionality

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //variables
 let gridSizeChoice;
+let rotationDegreesY = 0;
+let rotationDegreesX = 1;
 
 //html elements
+const root = document.querySelector(':root');
 const etchScreen = document.querySelector('.screen-container');
 const knobX = document.querySelector('.knob-x');
 const knobY = document.querySelector('.knob-y');
@@ -15,7 +18,7 @@ function cloneLoop() {
     cloned.classList.add('etch-tile');
     etchScreen.appendChild(cloned);
 };
-function populateScreen(rowSize = 64) {
+function populateScreen(rowSize = 16) {
     let tileSize = 800 / rowSize;
     document.documentElement.style.setProperty('--tileSize', `${tileSize}px`);
     let tileAmount = rowSize ** 2;
@@ -53,12 +56,15 @@ resetBtn.onclick = function() {
     } else {
         cleanUpScreen2()
         populateScreen()
+        gridSizeChoice = 16
     }
 let allTiles = document.querySelectorAll('.etch-tile');
 allTiles.forEach(etchTile => etchTile.addEventListener('mouseover', changeColor))
 allTiles.forEach(etchTile => etchTile.addEventListener('mouseenter', eventListen))
 
 allTiles.forEach(etchTile => etchTile.addEventListener('touchmove', changeColor2))
+allTiles.forEach(etchTile => etchTile.addEventListener('touchmove', eventListen2))
+
 }
 function changeColor2(e) {
     //console.log(e.changedTouches[0].clientX);
@@ -67,21 +73,66 @@ function changeColor2(e) {
    //console.log(tileChecker)
     if (tileChecker) {
     document.elementFromPoint(e.changedTouches[0].clientX, e.changedTouches[0].clientY).style.backgroundColor = 'black';
-    console.log(1)
     }
 }
+function eventListen2(e) {
+    console.log(e);
+}
 function eventListen(e) {
-    console.log('clientx ' + e.clientX);
-    console.log('clienty ' + e.clientY);
-
+    // console.log('clientx ' + e.clientX);
+    // console.log('clienty ' + e.clientY);
+    // console.log(compareOffsetLeft(e))
+    // console.log(compareOffsetTop(e))
+    let xDelta = compareOffsetLeft(e)
+    let yDelta = compareOffsetTop(e)
+    calculateRotation(xDelta, yDelta);
+    storeOffsetLeft(e);
+    storeOffsetTop(e);
+    //console.log(e.path[0].offsetLeft)
+    
 }
-let previousTileClientX;
-let previousTileClientY;
+let previousTileOffsetLeft = 0;
+let previousTileOffsetTop = 0;
 //two functions to return clientx and client y and store in variable
-function storeClientX(e) {
-    return previousTileClientX = e.clientX
+function storeOffsetLeft(e) {
+    if (isNaN(e.path[0].offsetLeft)) {
+        return previousTileOffsetTop = 1;
+    }
+    return previousTileOffsetLeft = e.path[0].offsetLeft
 }
-function storeClientY(e) {
-    return previousTileClientY = e.clientY
+function storeOffsetTop(e) {
+    return previousTileOffsetTop = e.path[0].offsetTop
 }
 //function to subtract x and y variables from new clientx and clienty
+function compareOffsetLeft(e) {
+    let xDelta = previousTileOffsetLeft - e.path[0].offsetLeft
+    if (isNaN(xDelta)) {return}
+    return xDelta
+}
+function compareOffsetTop(e) {
+    let yDelta = previousTileOffsetTop - e.path[0].offsetTop
+    return yDelta
+}
+function calculateRotation(x, y) {
+    let rotationValue;
+    if (y ** 2 > x ** 2) {
+        rotationValue = (Math.sqrt(gridSizeChoice) / 200) * y;
+        return rotateKnobY(rotationValue);
+    } else (y ** 2 < x ** 2) ;{
+        rotationValue = (Math.sqrt(gridSizeChoice) / 200) * (x * -1);
+        console.log(rotationValue)
+        return rotateKnobX(rotationValue);
+        
+    }
+}
+function rotateKnobY(rotationValue) {
+    rotationDegreesY += rotationValue * 45
+    root.style.setProperty("--rotateKnobY", (rotationDegreesY + 'deg'))
+    //console.log(rotationValue);
+}
+function rotateKnobX(rotationValue) {
+    rotationDegreesX += rotationValue * 45
+    root.style.setProperty("--rotateKnobX", (rotationDegreesX + 'deg'))
+    //console.log(rotationValue * 45);
+
+}
